2aaf47c70031df332dd43b6fa30f6e97f738f215
Adityaraj1612 <adityaraj10b@gmail.com>
2025-06-25 18:03:41 +0530

    Refactor Hero, Navbar, and Testimonials components for improved layout and responsiveness

--- src/components/HomePageComponent/Navbar.jsx
+++ src/components/HomePageComponent/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     >
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
-        <div className="text-4xl font-bold">Brahmastra</div>
+        <div className="text-2xl md:text-3xl font-bold">Brahmastra</div>
 
         {/* Menu Items */}
         <ul className="hidden md:flex space-x-8 font-medium text-lg">
